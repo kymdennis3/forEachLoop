d71fe84d2408fcccced7172f71976baae76cc953
kymdennis3 <kymdennis3@gmail.com>
2022-11-03 09:02:21 +0300

    commented out normal array and introduced ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*for each - traversing technique to iterate through the elements in an array/collection
+                     less steps, more readable
+                     less flexible
+         */
+          //String [] animals = {"dog", "cat", "hen", "cow"};
+
+        ArrayList<String> animals = new ArrayList<>();
+
+        animals.add("cows");
+        animals.add("cats");
+        animals.add("dogs");
+        animals.add("hen");
+
+          for(String i:animals){  //colon represents the word in - for every String index {in} array of animals
+        System.out.println(i);
     }
-}
+}}
